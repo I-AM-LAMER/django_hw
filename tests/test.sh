ef92fa7d9e54696117d3585ccafa64d91c6053d4
I-AM-LAMER <vadimtatarnikov300605@gmail.com>
2024-09-13 09:48:04 +0300

    fix workflows

---
+++ tests/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export POSTGRES_HOST=127.0.0.1
+export POSTGRES_PORT=5432
+export POSTGRES_USER=test
+export POSTGRES_PASSWORD=test
+export POSTGRES_DB=test
+
+python3 manage.py test $1
